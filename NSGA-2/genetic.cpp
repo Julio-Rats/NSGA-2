ca3c04bcf41e3df7729adcc946c49bc00e4f70aa
Rats <juliocesarmmc@hotmail.com>
2019-08-14 02:57:14 -0300

    First

---
+++ NSGA-2/genetic.cpp
@@ -0,0 +1,287 @@
+#include "genetic.hpp"
+
+using namespace std;
+
+void NSGA_II()
+{
+    vector<Indiviual>         sort_population;
+    vector<vector<Indiviual>> frontiers;
+    unsigned int number_frontiers;
+
+    ga_sol_population      = random_population(first_man);
+    unsigned int generation_current = 1;
+
+    while (generation_current <= ga_number_generation)
+    {
+        frontiers         = nodominated_sort();
+        sort_population   = crowding_distance_assigment(frontiers);
+        ga_sol_population = make_new_population(sort_population);
+        generation_current++;
+    }
+}
+
+Indiviual crossover(Indiviual parent1, Indiviual parent2)
+{
+    Indiviual new_person;
+
+
+    return new_person;
+}
+
+vector<Indiviual> make_new_population(vector<Indiviual> sort_population)
+{
+      vector<Indiviual> new_generation;
+
+
+      return new_generation;
+}
+
+void sort_value_object(vector<Indiviual>& sort_list, unsigned int funct)
+{
+      double value;
+      unsigned int pos_better;
+      Indiviual temp;
+      for (size_t i = 0; i < sort_list.size(); i++)
+      {
+          pos_better = i;
+          switch (funct)
+          {
+            case 0:
+                value = sort_list[i].wcrt;
+            break;
+            case 1:
+                value = sort_list[i].frames_burst;
+            break;
+            case 2:
+                value = sort_list[i].time_mean_burst;
+            break;
+          }
+          for (size_t j = 0; j < sort_list.size(); j++)
+          {
+              switch (funct)
+              {
+                case 0:
+                    if (value > sort_list[i].wcrt)
+                    {
+                        value = sort_list[i].wcrt;
+                        pos_better = j;
+                    }
+                break;
+                case 1:
+                    if (value > sort_list[i].frames_burst)
+                    {
+                        value = sort_list[i].frames_burst;
+                        pos_better = j;
+                    }
+                break;
+                case 2:
+                    if (value > sort_list[i].time_mean_burst)
+                    {
+                        value = sort_list[i].time_mean_burst;
+                        pos_better = j;
+                    }
+                break;
+              }
+          }
+          if (pos_better != i)
+          {
+              temp = sort_list[i];
+              sort_list[i] = sort_list[pos_better];
+              sort_list[pos_better] = temp;
+          }
+      }
+}
+
+void sort_distance(vector<Indiviual>& sort_list)
+{
+    double value;
+    unsigned int pos_better;
+    Indiviual temp;
+    for (size_t i = 0; i < sort_list.size(); i++)
+    {
+        value = sort_list[i].distance;
+        pos_better = i;
+        for (size_t j = 0; j < sort_list.size(); j++)
+        {
+            if (value < sort_list[j].distance)
+            {
+                value = sort_list[j].distance;
+                pos_better = j;
+            }
+        }
+        if (pos_better != i)
+        {
+            temp = sort_list[i];
+            sort_list[i] = sort_list[pos_better];
+            sort_list[pos_better] = temp;
+        }
+    }
+}
+
+vector<Indiviual> crowding_distance_assigment(vector<vector<Indiviual>> frontiers)
+{
+    for (vector<Indiviual>& front : frontiers)
+    {
+        for (Indiviual& person: front)
+            person.distance = 0;
+
+        for (size_t j = 0; j < number_funct_objective; j++)
+        {
+            sort_value_object(front, j);
+            front[0].distance = DBL_MAX;
+            front[front.size()-1].distance   = DBL_MAX;
+
+            for (int i = 1; i < front.size()-1; i++)
+                if (j == 0)
+                {
+                  front[i].distance += (front[i+1].wcrt)-(front[i-1].wcrt);
+                }
+                else if (j == 1)
+                {
+                  front[i].distance += (front[i+1].frames_burst)-(front[i-1].frames_burst);
+                }
+                else if (j == 2)
+                {
+                  front[i].distance += (front[i+1].time_mean_burst)-(front[i-1].time_mean_burst);
+                }
+        }
+    }
+    vector<Indiviual> sort_population;
+    unsigned int current_length_population = 0;
+    for (vector<Indiviual>& front : frontiers)
+    {
+      sort_distance(front);
+      for (Indiviual e: front)
+          if (current_length_population <= (ga_length_population/2))
+          {
+              sort_population.push_back(e);
+              current_length_population++;
+          }
+          else
+          {
+              return sort_population;
+          }
+    }
+    return sort_population;
+}
+
+vector<vector<Indiviual>> nodominated_sort()
+{
+    for (Indiviual& person: ga_sol_population)
+        run_simulation(person);
+
+    vector<vector<Indiviual>> frontiers(1);
+
+    for (Indiviual& person_dmt: ga_sol_population)
+    {
+        for (Indiviual& person_slv: ga_sol_population)
+        {
+            if (is_dominated(person_dmt, person_slv))
+            {
+                person_dmt.dominated.push_back(&person_slv);
+            }
+            else if (is_dominated(person_slv, person_dmt))
+            {
+                person_dmt.transcend++;
+            }
+        }
+        if (person_dmt.transcend == 0)
+        {
+            frontiers[0].push_back(person_dmt);
+        }
+    }
+    unsigned int i = 0;
+    vector<Indiviual> aux_dominates;
+    while (frontiers[i].size() != 0)
+    {
+        aux_dominates.clear();
+        for (Indiviual& person_frt: frontiers[i])
+          for (Indiviual* person_dmtd: person_frt.dominated)
+          {
+              person_dmtd->transcend--;
+              if (person_dmtd->transcend == 0)
+              {
+                  aux_dominates.push_back(*person_dmtd);
+              }
+          }
+        frontiers.push_back(aux_dominates);
+        i++;
+    }
+    frontiers.resize(frontiers.size()-1);
+    return frontiers;
+}
+
+bool is_dominated(Indiviual dmt, Indiviual slv)
+{
+    return(
+          ((dmt.wcrt <= slv.wcrt)&&(dmt.frames_burst <= slv.frames_burst)&&(dmt.time_mean_burst <= slv.time_mean_burst))
+           &&
+          ((dmt.wcrt < slv.wcrt)||(dmt.frames_burst < slv.frames_burst)||(dmt.time_mean_burst < slv.time_mean_burst))
+       );
+}
+
+vector<Indiviual> random_population(Indiviual person_reference)
+{
+    Indiviual          new_person;
+    vector<Indiviual>  new_population;
+    for (unsigned int i=0; i < ga_length_population; i++)
+    {
+        new_person.candb_solution = new Frame_CAN[length_frames];
+        if (!new_person.candb_solution)
+        {
+            std::cerr << "ERRO alocação de memoria para vetor de frames" << '\n';
+            exit(3);
+        }
+        for (unsigned int j=0; j < length_frames; j++)
+        {
+            new_person.candb_solution[j] = person_reference.candb_solution[j];
+            new_person.candb_solution[j].delay_time = (double)(rand()%((int) new_person.candb_solution[j].deadline_time));
+        }
+        new_population.push_back(new_person);
+    }
+    return new_population;
+}
+
+void run_simulation(Indiviual& person)
+{
+    simulator = new Simulator_CAN(person.candb_solution, length_frames);
+    simulator->run_simulation(TIME_SIMULATION);
+    // double delay_sum = 0;
+    // for (size_t i = 0; i < length_frames; i++)
+    // {
+    //     delay_sum += solucao.candb_solution[i].delay_time;
+    // }
+    // delay_sum *= 100;
+    person.transcend       =  0;
+    person.wcrt            =  simulator->wcrt;
+    person.frames_burst    =  simulator->frames_burst;
+    person.time_mean_burst =  simulator->time_mean_burst;
+    delete(simulator);
+}
+
+int main(int argc, char const *argv[])
+{
+    srand(time(NULL));
+    FILE* arq  = fopen(argv[1], "r");
+    if (!arq)
+    {
+        std::cerr << "\nERRO AO ABRIR O ARQUIVO\n" << '\n';
+        exit(3);
+    }
+    Frame_CAN* frames = get_CANDB(arq, length_frames);
+    first_man.candb_solution = frames;
+
+    ga_length_population   = 20;
+    number_funct_objective = 3;
+    ga_sol_population      = random_population(first_man);
+    vector<vector<Indiviual>> aux = nodominated_sort();
+    vector<Indiviual> aux2        = crowding_distance_assigment(aux);
+
+    for (vector<Indiviual> e: aux)
+        std::cout << e.size() << '\n';
+
+    // std::cout << "leng " << aux2.size() << '\n';
+    std::cout << aux2[0].distance << '\n';
+    std::cout << aux2[aux[0].size()-1].distance << '\n';
+    return 0;
+}
---
+++ NSGA-2/genetic.hpp
@@ -0,0 +1,67 @@
+#ifndef  NSGA_2
+#define   NSGA_2
+
+#include <iostream>
+#include <cstdlib>
+#include <list>
+#include <cfloat>
+#include <vector>
+#include <fstream>
+#include "./../Simulador deterministico/simulador.hpp"
+
+#define     LENGTH_CHAR           64
+#define     TIME_SIMULATION       12000
+
+/******************************************************************
+
+Estrutura (classe) da solução do problema
+
+*******************************************************************/
+
+class Indiviual
+{
+  public:
+      double          wcrt;                  //  Pior tempo de atraso cumulado
+      double          frames_burst;          //  Numero maximo de frames ocorrido em  um Burst
+      double          time_mean_burst;       //  Tempo medio dos bursts
+      Frame_CAN*      candb_solution;        //  Conjunto de frames
+      unsigned int    transcend;
+      double          distance;
+      std::vector<Indiviual*> dominated;
+};
+
+
+/******************************************************************
+
+Variaveis de controle do algoritimo genetico
+
+*******************************************************************/
+
+unsigned int          length_frames;      // Numero de frames na solução
+unsigned int          length_frontier;    // Numero de soluções na nuvem de soluções
+unsigned int          ga_length_population;
+unsigned int          ga_number_generation;
+double                ga_prob_mution;
+unsigned int          number_funct_objective;
+std::vector<Indiviual>  ga_sol_population;     // Conjunto com todas as soluções (population)
+
+Simulator_CAN* simulator;
+Indiviual      first_man;
+
+char            psa_arq_result[LENGTH_CHAR];
+/******************************************************************
+
+Escopos das funções implementadas
+
+*******************************************************************/
+
+void NSGA_II();
+std::vector<std::vector<Indiviual>> nodominated_sort();
+std::vector<Indiviual> make_new_population (std::vector<Indiviual> sort_population);
+std::vector<Indiviual> crowding_distance_assigment(std::vector<std::vector<Indiviual>> frontiers);
+bool is_dominated(Indiviual dmt, Indiviual slv);
+std::vector<Indiviual> random_population(Indiviual person_reference);
+void run_simulation(Indiviual& person);
+
+
+#endif
